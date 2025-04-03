e74cfd262aa1fb2a954f9939be9a7167156d5c0e
Duvini <it22884510@my.sliit.lk>
2025-04-03 11:35:35 +0530

    feat: add Home page with post feed and comment input components

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import Sidebar from './components/Sidebar/sidebar';
+import PostCard from './components/PostCard/postCard';
+import CommentInput from './components/CommentInput/commentInput';
+
+export { Sidebar, PostCard,CommentInput};
