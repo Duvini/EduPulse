f6515abaaa72345f1cbfd19dd20da3f5b03061e7
Ranketh2004 <it22136824@my.sliit.lk>
2025-04-10 13:11:07 +0530

    Remove old gradle backend and setup with maven

---
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/EduPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.edupulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduPulseBackendApplication.class, args);
+    }
+
+}
