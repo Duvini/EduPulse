24eafcbaed4d3fd83f8df5e85384153bee733ae3
Duvini <it22884510@my.sliit.lk>
2025-04-15 16:06:07 +0530

    Add new video upload: dab6524f-206d-4998-bc96-4ec72f3c3d65.mp4

--- EduPulseBackend/src/main/java/com/example/edupulse_backend/service/SkillPostService.java
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/service/SkillPostService.java
@@ -1,4 +1,19 @@
 package com.example.edupulse_backend.service;
 
+import com.example.edupulse_backend.model.SkillPost;
+import com.example.edupulse_backend.payload.response.ResponseDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface SkillPostService {
+    ResponseDto createSkillPost(String userId, String profilePhotoUrl, String description, List<String> tags, MultipartFile[] files);
+
+    ResponseDto getPostsByUserId(String userId);
+
+    ResponseDto getPostsByFollowedUserIds(List<String> followedUserIds);
+
+    ResponseDto updateSkillPost(String id, SkillPost post);
+
+    ResponseDto deleteSkillPost(String id);
 }
