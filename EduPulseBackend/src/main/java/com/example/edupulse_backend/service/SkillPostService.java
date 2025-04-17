280f732ce1bfa1beb5a4e19bace7d531c97819e4
Duvini <it22884510@my.sliit.lk>
2025-04-17 10:17:39 +0530

    Refactor code structure for improved readability and maintainability

--- EduPulseBackend/src/main/java/com/example/edupulse_backend/service/SkillPostService.java
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/service/SkillPostService.java
@@ -1,17 +1,17 @@
 package com.example.edupulse_backend.service;
 
-import com.example.edupulse_backend.model.SkillPost;
 import com.example.edupulse_backend.payload.response.ResponseDto;
+import com.example.edupulse_backend.model.SkillPost;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface SkillPostService {
-    ResponseDto createSkillPost(String userId, String profilePhotoUrl, String description, List<String> tags, MultipartFile[] files);
+    ResponseDto createSkillPost(String userId, String userName, String profilePhotoUrl, String description, List<String> tags, MultipartFile[] files);
 
     ResponseDto getPostsByUserId(String userId);
 
-    ResponseDto getPostsByFollowedUserIds(List<String> followedUserIds);
+    ResponseDto getPostsByFollowedUserIds(List<String> userIds);
 
     ResponseDto updateSkillPost(String id, SkillPost post);
 
