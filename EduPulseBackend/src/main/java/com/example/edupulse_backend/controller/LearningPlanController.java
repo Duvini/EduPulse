82cfcb480d25fc47012bcb3cf063fa477d7d9e00
Ranketh2004 <it22136824@my.sliit.lk>
2025-04-20 23:45:06 +0530

    Update code to new folder structure

--- EduPulseBackend/src/main/java/com/example/edupulse_backend/controller/LearningPlanController.java
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/controller/LearningPlanController.java
@@ -1,20 +1,17 @@
 package com.example.edupulse_backend.controller;
 
 import com.example.edupulse_backend.model.LearningPlan;
+import com.example.edupulse_backend.payload.response.ResponseDto;
 import com.example.edupulse_backend.service.LearningPlanService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -25,108 +22,75 @@ public class LearningPlanController {
     @Autowired
     private final LearningPlanService service;
 
-    @GetMapping("/sample")
-    public String sample() {
-        return "Hello world";
-    }
-
     //create a learning plan
     @PostMapping("/create")
-    public ResponseEntity<?> createPlan(@RequestBody LearningPlan plan){
-        try{
-            LearningPlan saved = service.create(plan);
-            return ResponseEntity.status(201).body(toModel(saved));
-        }catch(Exception e){
-            return ResponseEntity.status(500).body("Error creating a plan: " + e.getMessage());
-        }
-
+    public ResponseDto createPlan(@RequestBody LearningPlan plan) {
+        return service.createLearningPlan(plan);
     }
 
     //Get All Learning plans
     @GetMapping
-    public ResponseEntity<?> getAllPlans(){
-        try{
-            List<EntityModel<LearningPlan>> plans = service.getAll().stream()
-                    .map(this::toModel)
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(plans);//200
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error retrieving plans: "+e.getMessage());
-        }
+    public ResponseDto getPlans() {
+        return service.getAllLearningPlans();
     }
+
+    //Get learning plans on creator id
+    @GetMapping("{userid}/plans")
+    public ResponseDto getLearningPlanOfUser(
+            @PathVariable String userid
+    ) {
+        return service.getLearningPlanOfUser(userid);
+    }
+
 
     //Get a single learning plan
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getPlan(@PathVariable String id){
-        try{
-            Optional<LearningPlan> plan  = service.getById(id);
-            return plan.map(p -> ResponseEntity.ok(toModel(p)))
-                    .orElse(ResponseEntity.notFound().build());
-        }catch (Exception e){
-            return ResponseEntity.status(500).body("Error retrieving a plan: "+e.getMessage());
-        }
-    }
-
-    //Update Task status
-    @PutMapping("/{id}/tasks/{index}")
-    public ResponseEntity<?> updateTaskStatus(
-            @PathVariable String id,
-            @PathVariable int index,
-            @RequestParam boolean completed
-    ){
-        try{
-            Optional<LearningPlan> updated = service.updateTaskStatus(id, index, completed);
-            return updated.map(p -> ResponseEntity.ok(toModel(p))) //200
-                    .orElse(ResponseEntity.notFound().build());
-        }catch(Exception e) {
-            return ResponseEntity.status(500).body("Error updating task status: " + e.getMessage());
-        }
+    @GetMapping("/{planId}")
+    public ResponseDto getPlan(
+            @PathVariable String planId
+    ) {
+        return service.getLearningPlan(planId);
     }
 
     //Update learning plans
     @PatchMapping("/update/{planId}")
-    public ResponseEntity<?> updateLearningPlan(
+    public ResponseDto updateLearningPlan(
             @PathVariable String planId,
             @RequestBody LearningPlan planUpdates
     ){
-            try{
-                Optional<LearningPlan> updatePlan = service.updatePlan(planId, planUpdates);
-                return updatePlan.map(p -> ResponseEntity.status(201).body(toModel(p)))
-                        .orElse(ResponseEntity.notFound().build());
-            }
-            catch(Exception e){
-                return ResponseEntity.status(500).body("Error updating a plan: "+e.getMessage());
-            }
+            return service.updateLearningPLan(planId, planUpdates);
     }
 
     //delete a learning plan
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deletePlan(@PathVariable String id){
-        try{
-            Optional<LearningPlan> plan = service.getById(id);
-            if(plan.isPresent()){
-                service.delete(id);
-                return ResponseEntity.noContent().build();//204
-            }else{
-                return ResponseEntity.notFound().build();//404
-            }
-        }catch(Exception e){
-            return ResponseEntity.status(500).body("Error deleting plan: "+e.getMessage());
-        }
+    @DeleteMapping("/delete/{planId}")
+    public ResponseDto deletePlan(
+            @PathVariable String planId
+    ){
+        return service.deleteLearningPlan(planId);
     }
 
-    // HATEOAS model builder
-    private EntityModel<LearningPlan> toModel(LearningPlan plan) {
-        return EntityModel.of(plan,
-                linkTo(methodOn(LearningPlanController.class).getPlan(plan.getId())).withSelfRel(),
-                linkTo(methodOn(LearningPlanController.class).getAllPlans()).withRel("all-plans"),
-                linkTo(methodOn(LearningPlanController.class).updateTaskStatus(plan.getId(), 0, true)).withRel("mark-task-complete"),
-                linkTo(methodOn(LearningPlanController.class).updateLearningPlan(plan.getId(),null)).withRel("update-plan"),
-                linkTo(methodOn(LearningPlanController.class).createPlan(null)).withRel("create"),
-                linkTo(methodOn(LearningPlanController.class).deletePlan(plan.getId())).withRel("delete")
-
-        );
+    //update task status
+    @PutMapping("/{planId}/tasks/{index}")
+    public ResponseDto updateTaskStatus(
+            @PathVariable String planId,
+            @PathVariable int index,
+            @RequestParam boolean isCompleted
+    ){
+        return service.updateTaskStatus(planId, index, isCompleted);
     }
+
+
+//    // HATEOAS model builder
+//    private EntityModel<LearningPlan> toModel(LearningPlan plan) {
+//        return EntityModel.of(plan,
+//                linkTo(methodOn(LearningPlanController.class).getPlan(plan.getId())).withSelfRel(),
+//                linkTo(methodOn(LearningPlanController.class).getAllPlans()).withRel("all-plans"),
+//                linkTo(methodOn(LearningPlanController.class).updateTaskStatus(plan.getId(), 0, true)).withRel("mark-task-complete"),
+//                linkTo(methodOn(LearningPlanController.class).updateLearningPlan(plan.getId(),null)).withRel("update-plan"),
+//                linkTo(methodOn(LearningPlanController.class).createPlan(null)).withRel("create"),
+//                linkTo(methodOn(LearningPlanController.class).deletePlan(plan.getId())).withRel("delete")
+//
+//        );
+//    }
 
 
 }
--- EduPulseBackend/src/main/java/com/example/edupulse_backend/model/LearningPlan.java
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/model/LearningPlan.java
@@ -1,6 +1,9 @@
 package com.example.edupulse_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,10 +22,21 @@ import java.util.List;
 public class LearningPlan {
     @Id
     private String id;
+
     private String creatorId;
+
+    @NotBlank(message="Title is required")
+    @Size(min=3,max=50, message = "Title must be within 3 and 50 characters")
     private String title;
+
+    @NotBlank(message="Description is required")
+    @Size(max=150, message = "Can't exceed 150 characters")
     private String description;
+
+    @NotNull(message = "Tasks list cannot be null")
+    @Size(min=1, message = "At least one task should be added")
     private List<Task> tasks;
+
     @CreatedDate
     private LocalDateTime createdAt;
 
