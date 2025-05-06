de91cb709cbaaac6d4d58a0502ebb57405aa3f1d
Ranketh2004 <it22136824@my.sliit.lk>
2025-05-06 09:19:43 +0530

    commit to checkout

--- EduPulseBackend/src/main/java/com/example/edupulse_backend/controller/LearningPlanController.java
+++ EduPulseBackend/src/main/java/com/example/edupulse_backend/controller/LearningPlanController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/plans")
 @RequiredArgsConstructor
